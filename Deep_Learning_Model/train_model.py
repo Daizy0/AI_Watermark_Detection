03535b6753586819ee6a6bb223951a066b54717a
Daizy0 <132797720+Daizy0@users.noreply.github.com>
2025-03-17 01:09:11 +0530

    Update train_model.py

--- Deep_Learning_Model/train_model.py
+++ Deep_Learning_Model/train_model.py
@@ -1,1 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class WatermarkNet(nn.Module):
+    def __init__(self):
+        super(WatermarkNet, self).__init__()
+        self.conv = nn.Conv2d(1, 16, 3, stride=1)
+        self.fc = nn.Linear(16, 1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(x.size(0), -1)
+        return torch.sigmoid(self.fc(x))
 
