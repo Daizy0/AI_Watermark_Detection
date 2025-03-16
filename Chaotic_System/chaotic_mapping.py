db30ebc9661cfca14dd754e744c2cc5ca9b0d6e0
Daizy0 <132797720+Daizy0@users.noreply.github.com>
2025-03-17 01:08:48 +0530

    Update chaotic_mapping.py

--- Chaotic_System/chaotic_mapping.py
+++ Chaotic_System/chaotic_mapping.py
@@ -1,1 +1,14 @@
+import numpy as np
+
+def chaotic_map(size):
+    x = 0.5
+    y = 0.5
+    chaotic_sequence = []
+    
+    for _ in range(size):
+        x = 3.9 * x * (1 - x)  
+        y = 3.9 * y * (1 - y)  
+        chaotic_sequence.append((x + y) % 1)  
+        
+    return chaotic_sequence
 
